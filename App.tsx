1c6361bf0055e1e0c4450b21b5552947d8b68717
Benjamin mumbita <benjaminmumbita10@gmail.com>
2023-10-31 09:21:18 +0700

    navigation end

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import React from 'react-native';
+
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/navigation/LoginScreen';
 import Signup from './src/navigation/SignupScreen';
@@ -11,28 +13,61 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/navigation/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeStacknavigatorParamList } from './global';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Stack = createStackNavigator<HomeStacknavigatorParamList>();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-         <Stack.Navigator
-         screenOptions={{
-          headerShown: false,
-         gestureEnabled: false,
 
-        }}
-         
-         >
-           <Stack.Screen name="Onboard" component={OnboardScreen} />
-           <Stack.Screen name="Login" component={LoginScreen} />
-           <Stack.Screen name="Signup" component={SignupScreen} />
-           <Stack.Screen name="Forgot" component={Forgot} />
-         </Stack.Navigator>
+  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch) {
+    return  (
+      <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+       headerShown: false,
+      gestureEnabled: false,
+
+     }}
+      
+      >
+        <Stack.Screen name="Onboard" component={OnboardScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Forgot" component={Forgot} />
+      </Stack.Navigator>
+ </NavigationContainer>
+
+    );
+  } else {
+   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+    
+
+      </Stack.Navigator>
     </NavigationContainer>
-   
-  );
+   )
+  }
 }
+
 
 export default App;
